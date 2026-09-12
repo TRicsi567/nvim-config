f2b893d266a490986474ef2b2e9f7c5e0cee03b2
Richard David <torokricsi.lkg@gmail.com>
2026-09-12 10:46:51 +0200

    feat: treesitter filetype autocommand added

--- pack/nvim-gombolo/start/core/lua/nvim-gombolo/core/autocommands.lua
+++ pack/nvim-gombolo/start/core/lua/nvim-gombolo/core/autocommands.lua
@@ -9,7 +9,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('tree-sitter-file-type', { clear = true }),
   pattern = '*',
-  callback = function()
-    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  callback = function(ev)
+    if not vim.treesitter.language.add(ev.match) then
+      return
+    end
+
+    vim.treesitter.start(ev.buf, ev.match)
+
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
   end,
 })
